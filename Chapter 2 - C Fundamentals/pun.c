331b35aecb824a2e464cd9929cd732417ab8f54b
Mohaimen Hassan <mohaimenhassan08@gmail.com>
2021-11-07 11:54:16 -0500

    Finished Ch2.2

---
+++ Chapter 2 - C Fundamentals/pun.c
@@ -0,0 +1,15 @@
+/*
+* Name: pun.c
+* Purpose: Prints a bad pun.
+* Author: Mohaimen Hassan 
+*/
+
+// Or this works too.
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("To C, or not to C: that is the question.\n");
+    return 0;
+}
