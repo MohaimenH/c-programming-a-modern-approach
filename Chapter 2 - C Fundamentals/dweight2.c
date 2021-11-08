a08667daa006e785b3c66681d2a975bb2258daf1
Mohaimen Hassan <mohaimenhassan08@gmail.com>
2021-11-08 15:44:50 -0500

    Macro definitions

--- Chapter 2 - C Fundamentals/dweight2.c
+++ Chapter 2 - C Fundamentals/dweight2.c
@@ -2,6 +2,9 @@
 
 #include <stdio.h>
 
+#define INCHES_PER_POUND 166               // Macro definition - Pre-processor replaces each instance of this with the value
+#define RECIPROCAL_OF_PI (1.0f / 3.14159f) // Can also include expression in brackets
+
 int main()
 {
     int height, length, width, volume, weight; // Here, ONLY weight is 1, the others are initialized as 0.
@@ -14,7 +17,7 @@ int main()
     scanf("%d", &width);
 
     volume = height * length * width;
-    weight = (volume + 165) / 166;
+    weight = (volume + 165) / INCHES_PER_POUND;
 
     printf("Dimensions: %dx%dx%d\n", length, width, height);
     printf("Volume (cubic inches): %d\n", volume);
